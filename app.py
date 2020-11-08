0f147f2467e997ea2c815ef352ac364d160a8461
Jahnvi Gupta <43217070+jahnvigupta@users.noreply.github.com>
2020-11-08 15:19:49 +0530

    Added intergroup stats

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ from tkinter import filedialog
 from matplotlib import pyplot as plt
 import numpy as np
 import statistics as st
+import gpxpy
+import pandas as pd
+import math
+import os
 
 # Creating the class of the application
 class Application(Tk):
@@ -17,7 +21,7 @@ class Application(Tk):
         self.geometry('550x1520')
         self.grid()
         
-
+        self.groupNumber=""
         self.dir_location=""
         self.dist=""
         self.avgtime=""
@@ -39,6 +43,11 @@ class Application(Tk):
         self.startPoint4=""
         self.endPoint4=""
         self.rides4=""
+
+        self.groups = {}
+
+        #Threshold to compare 2 points
+        self.threshold = 1
 
         # File labels 
         self.file_label=Label(self, text = "Directory Not Chosen ." + self.dir_location)
@@ -94,6 +103,8 @@ class Application(Tk):
         self.third = Label(self, text="\n3rd Point")
         self.third.config(font=("Helvetica", 18))
         self.third.pack()
+        self.group_no = Entry(self, textvariable=self.groupNumber)
+        self.group_no.pack()
         self.latitude = Entry(self, textvariable=self.lat)
         self.latitude.pack()
         self.longitude = Entry(self, textvariable=self.long)
@@ -131,7 +142,44 @@ class Application(Tk):
     def set_dir_location(self):
         self.dir_location = filedialog.askdirectory()
         self.file_label.config(text = "File: " + self.dir_location)
-        self.file_function()
+        self.groups = self.group(self.dir_location)
+        
+        self.startPoint1 = "lat: "+str(self.groups[0][0].loc[0]['lat']) + "\tLong: "+str(self.groups[0][0].loc[0]['lon'])
+        self.startPt1.config(text="Start Point 1: " + self.startPoint1)
+        
+        self.endPoint1 = "lat: "+str(self.groups[0][0].loc[len(self.groups[0][0])-1]["lat"]) + "\tLong: "+str(self.groups[0][0].loc[len(self.groups[0][0])-1]["lon"])
+        self.endPt1.config(text="End Point 1: " + self.endPoint1)
+        
+        self.numOfRides1.config(text="Number of Rides: "+str(len(self.groups[0])))
+        
+        if (len(self.groups) > 1):
+            self.startPoint2 = "lat: "+str(self.groups[1][0].loc[0]['lat']) + "\tLong: "+str(self.groups[1][0].loc[0]['lon'])
+            self.startPt2.config(text="Start Point 2: " + self.startPoint2)
+            
+            self.endPoint2 = "lat: "+str(self.groups[1][0].loc[len(self.groups[1][0])-1]["lat"]) + "\tLong: "+str(self.groups[1][0].loc[len(self.groups[1][0])-1]["lon"])
+            self.endPt2.config(text="End Point 2: " + self.endPoint2)
+            
+            self.numOfRides2.config(text="Number of Rides: "+str(len(self.groups[1])))
+                                    
+        if (len(self.groups) > 2 ):
+            self.startPoint3 = "lat: "+str(self.groups[2][0].loc[0]['lat']) + "\tLong: "+str(self.groups[2][0].loc[0]['lon'])
+            self.startPt3.config(text="Start Point 3: " + self.startPoint3)
+            
+            self.endPoint3 = "lat: "+str(self.groups[2][0].loc[len(self.groups[2][0])-1]["lat"]) + "\tLong: "+str(self.groups[2][0].loc[len(self.groups[2][0])-1]["lon"])
+            self.endPt3.config(text="End Point 3: " + self.endPoint3)
+            
+            self.numOfRides3.config(text="Number of Rides: "+str(len(self.groups[2])))
+                                    
+        if (len(self.groups) > 3 ):
+            self.startPoint4 = "lat: "+str(self.groups[3][0].loc[0]['lat']) + "\tLong: "+str(self.groups[3][0].loc[0]['lon'])
+            self.startPt4.config(text="Start Point 4: " + self.startPoint4)
+            
+            self.endPoint4 = "lat: "+str(self.groups[3][0].loc[len(self.groups[3][0])-1]["lat"]) + "\tLong: "+str(self.groups[3][0].loc[len(self.groups[3][0])-1]["lon"])
+            self.endPt4.config(text="End Point 4: " + self.endPoint4)
+            
+            self.numOfRides4.config(text="Number of Rides: "+str(len(self.groups[3])))
+                                
+        
 
     def speedVsDate(self):
         pass
@@ -143,9 +191,160 @@ class Application(Tk):
         pass
 
     def resolve_third_point(self):
+        #for group in self.groups[self.groupNumber]:
+        self.startPoint4 = "lat: "+str(self.groups[3][0].loc[0]['lat']) + "\tLong: "+str(self.groups[3][0].loc[0]['lon'])
+        self.startPt4.config(text="Start Point 4: " + self.startPoint4)
         pass
+
+
+    #Takes file as input and returns the file data as a dataframe
+    def gpx_dataframe(self, file):
+        gpx_file = open(file, 'r')
+        gpx = gpxpy.parse(gpx_file)
+        df = pd.DataFrame(columns=['lat', 'lon', 'ele', 'time'])
+        for segment in gpx.tracks[0].segments: # all segments
+            data = segment.points
+            for point in data:
+                df = df.append({'lon': point.longitude, 'lat' : point.latitude, 'ele' : point.elevation, 'time' : point.time}, ignore_index=True)
+        return df
+
+    def dis_points(self, lat1, lon1, lat2, lon2):
+        R = 6373.0
+        lat1 = math.radians(lat1)
+        lon1 = math.radians(lon1)
+        lat2 = math.radians(lat2)
+        lon2 = math.radians(lon2)
+        
+        dlon = lon2 - lon1
+        dlat = lat2 - lat1
+        
+        a = math.sin(dlat / 2)**2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon / 2)**2
+        c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+        distance = R * c
+        return distance
+
+
+    #Function returns true if 2 dataframes have same starting and ending point
+    def comparedf(self, df1, df2):
+        start1_lat = df1.loc[0]["lat"]
+        start1_long = df1.loc[0]["lon"]
+        start2_lat = df2.loc[0]["lat"]
+        start2_long = df2.loc[0]["lon"]
+        end1_lat = df1.loc[len(df1)-1]["lat"]
+        end1_long = df1.loc[len(df1)-1]["lon"]
+        end2_lat = df2.loc[len(df2)-1]["lat"]
+        end2_long = df2.loc[len(df2)-1]["lon"]
+
+        if(self.dis_points(start1_lat,start1_long,start2_lat,start2_long)<=self.threshold and self.dis_points(end1_lat,end1_long,end2_lat,end2_long)<=self.threshold):
+            return True
+        else:
+            return False
+
+    #Function to put routes with same satarting and ending point in one group
+    def group(self, path_to_dir):
+        group = {}
+        i = 0
+        for file in os.listdir(path_to_dir):
+            path = (str(path_to_dir+"/"+file))
+            print(path)
+            df = self.gpx_dataframe(path)
+
+            flag = True
+            keys = group.keys()
+            for j in (keys):
+                if(self.comparedf(group[j][0],df)):
+                    group[j].append(df)
+                    flag = False
+                    break
+            if(flag==True):
+                g = [df]
+                group[i] = g   
+                i = i+1    
+        return group
+
+        #Function to calculate total time of ride
+        def time(self, df):
+            time_start = df.loc[0]["time"]
+            time_end = df.loc[len(df)-1]["time"]
+            return time_end - time_start
+
+        #Function to calculate distance travelled in a ride
+        def route_len(self, df):
+            distance = 0
+            for i in range(len(df)-1):
+                long1 = df.loc[i]["lon"] 
+                long2 = df.loc[i+1]["lon"] 
+                lat1 = df.loc[i]["lat"] 
+                lat2 = df.loc[i+1]["lat"] 
+                R = 6373.0
+                lat1 = math.radians(lat1)
+                lon1 = math.radians(long1)
+                lat2 = math.radians(lat2)
+                lon2 = math.radians(long2)
+
+                dlon = lon2 - lon1
+                dlat = lat2 - lat1
+
+                a = math.sin(dlat / 2)**2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon / 2)**2
+                c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+                d = R * c
+
+                distance = distance + d
+                
+            return distance
+
+       
+
+        #Function to check if given latitude and longitude are part of a dataframe
+        def point(self, df, long, lat):
+            for i in range(len(df)):
+                if(self.dis_points(df.loc[i]["lat"],df.loc[i]["lon"],lat,long)<=self.threshold):
+                    return True
+            return False
+
+        def dateTime(self, groups):
+            for i in range(len(groups)):
+                plt.figure()
+                plt.title("Time taken on different days for longitude: ",groups[i][0].loc[0]["lon"],"and latitude: ",groups[i][0].loc[0]["lat"])
+                date=[]
+                time=[]
+                for j in range(len(groups[i])):
+                    date.append(groups[i][j].loc[0]["time"].date())
+                    time.append(groups[i][j].loc[0]["time"].time())
+                    plt.scatter(date,time)
+                    plt.xlabel("Date")
+                    plt.ylabel("Time")
+            
+        def eleTime(self, groups):
+            for i in range(len(groups)):
+                plt.figure()
+                plt.title("Time taken on different days for longitude: ",groups[i][0].loc[0]["lon"],"and latitude: ",groups[i][0].loc[0]["lat"])
+                date=[]
+                time=[]
+                for j in range(len(groups[i])):
+                    date.append(groups[i][j].loc[0]["time"].date())
+                    time.append(groups[i][j].loc[0]["time"].time())
+                    plt.scatter(date,time)
+                    plt.xlabel("Date")
+                    plt.ylabel("Time")
+            
+        def highest_ele(self, df):
+            return max(df["ele"])
+
+        def lowest_ele(self, df):
+            return max(df["ele"])
 
 # Create and run the application
 app = Application()
 app.title("Statistics For the File")
 app.mainloop()
+
+
+
+
+
+
+
+
+
+# groups = group("/home/jahnvi/Downloads/LAP_Main Project/rider3")
